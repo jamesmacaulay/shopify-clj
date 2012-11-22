5d8f6ab60884f36177d5a9743d379262a93e98c3
James MacAulay <james@shopify.com>
2012-11-21 20:39:10 -0500

    move ring-mock to a dev dependency

--- shopify-friend/project.clj
+++ shopify-friend/project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.cemerick/friend "0.1.2"]
-                 [clj-http "0.5.6"]
-                 [ring-mock "0.1.3"]])
+                 [clj-http "0.5.6"]]
+  :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}})
