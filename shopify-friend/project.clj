c8bae72f8934e16a8e432755434640537ced9cf2
James MacAulay <james@shopify.com>
2012-11-21 21:12:20 -0500

    use com.shopify group-id

--- shopify-friend/project.clj
+++ shopify-friend/project.clj
@@ -1,4 +1,4 @@
-(defproject shopify-friend "0.1.0-SNAPSHOT"
+(defproject com.shopify/shopify-friend "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
