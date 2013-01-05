9d19d63806dd95b25caf85251a00546519f368c4
James MacAulay <james@shopify.com>
2013-01-05 13:24:44 -0500

    bump clj-http to 0.6.3

--- shopify-friend/project.clj
+++ shopify-friend/project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.cemerick/friend "0.1.2"]
-                 [clj-http "0.5.6"]]
+                 [clj-http "0.6.3"]]
   :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}})
--- shopify-resources/project.clj
+++ shopify-resources/project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-http "0.5.6"]])
+                 [clj-http "0.6.3"]])
