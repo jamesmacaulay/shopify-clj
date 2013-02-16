9348fab219fc194ee94df25a6640131a6b85dcd8
James MacAulay <james@shopify.com>
2013-02-16 03:35:55 -0500

    test multiple clojures

--- shopify-resources/project.clj
+++ shopify-resources/project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-http "0.6.3"]])
+  :dependencies [[clj-http "0.6.3"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC16"]]}}
+  :aliases {"all" ["with-profile" "1.3:dev:1.5"]})
