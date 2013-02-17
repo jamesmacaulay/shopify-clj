a02cf79a97c381c601621624f85e77e1836d3e4a
James MacAulay <james@shopify.com>
2013-02-17 15:12:40 -0500

    extract shopify.resources.client-middleware

--- shopify-resources/src/shopify/resources.clj
+++ shopify-resources/src/shopify/resources.clj
@@ -3,176 +3,12 @@
   (:use [shopify.util :only [name-str partition-keys]])
   (:require [clojure.string :as str]
             [clojure.set :as set]
-            clj-http.core
-            clj-http.client
-            clj-http.links
+            [shopify.resources.client-middleware :as middleware]
             clj-http.util))
-
-(defn params->query-string
-  "Builds Rails-style nested query strings from param maps."
-  [data & [prefix]]
-  (cond
-    (sequential? data)
-    (->> data
-         (map (fn [item]
-                (params->query-string
-                  item
-                  (str prefix "[]"))))
-         (str/join \&))
-    (map? data)
-    (->> data
-         (map (fn [[k v]]
-                (params->query-string
-                  v
-                  (let [k (clj-http.util/url-encode (name k))]
-                    (if prefix (str prefix \[ k \]) k)))))
-         (str/join \&))
-    data
-    (->> (name-str data)
-         clj-http.util/url-encode
-         (str prefix (when prefix \=)))
-    :else
-    prefix))
-
-(defn wrap-query-params
-  "Middleware which converts a `:query-params` map into a Rails-style nested `:query-string`."
-  [client]
-  (fn [{:keys [query-params] :as req}]
-    (if query-params
-      (client (-> req
-                  (dissoc :query-params)
-                  (assoc
-                    :query-string
-                    (params->query-string query-params))))
-      (client req))))
-
-(defn wrap-json-format
-  "Middleware which forces JSON for everything, and appends `.json` to paths if it isn't already there."
-  [client]
-  (fn [req]
-    (let [req (assoc req
-                :accept :json
-                :as :json
-                :content-type :json)]
-      (if-let [uri (:uri req)]
-        (client (assoc req :uri (str/replace uri #"(?<!\.json)$" ".json")))
-        (client req)))))
-
-(defn wrap-access-token
-  "Middleware converting an `:access-token` option into the appropriate auth header."
-  [client]
-  (fn [req]
-    (if-let [token (:access-token req)]
-      (client (-> req
-                  (dissoc :access-token)
-                  (assoc-in [:headers "x-shopify-access-token"] token)))
-      (client req))))
-
-(defn wrap-ssl
-  "Middleware defaulting requests to SSL."
-  [client]
-  (fn [req]
-    (if-let [scheme (:scheme req)]
-      (client req)
-      (client (-> req
-                  (assoc :scheme :https))))))
-
-(defn wrap-shop
-  "Middleware converting a `:shop` option into `:server-name`."
-  [client]
-  (fn [req]
-    (if-let [shop (:shop req)]
-      (client (-> req
-                  (dissoc :shop)
-                  (assoc :server-name shop)))
-      (client req))))
-
-(defn wrap-retry-on-throttle-errors
-  "Middleware which retries a request if it's being throttled.
-
-Request options:
-
-* `:retry-on-throttle-errors`: whether to retry throttled requests (default `true`)
-* `:throttle-retry-delay`: how many seconds to wait between throttle retries (default `60`)
-* `:max-throttle-retries`: how many times to retry before giving up and returning the error response (default `12`)
-
-In the response:
-
-* `:throttle-retry-count`: number of retries (key is absent when there were no retries)"
-  [client]
-  (fn [req]
-    (if (= false (:retry-on-throttle-errors req))
-      (client req)
-      (let [wait-seconds (:throttle-retry-delay req 60)
-            max-retries (:max-throttle-retries req 12)]
-        (loop [retries 0]
-          (let [response (client req)]
-            (if (or
-                  (not= 429 (:status response))
-                  (= retries max-retries))
-              (if (= retries 0)
-                response
-                (assoc response :throttle-retry-count retries))
-              (do
-                (Thread/sleep (* wait-seconds 1000))
-                (recur (+ 1 retries))))))))))
-
-(def ^:private query-string-methods
-  #{:get :head :delete})
-
-(defn wrap-generic-params-key
-  "Middleware which renames `:params` to `:query-params` when method is GET, HEAD, or DELETE. For other methods, `:params` becomes `:form-params`."
-  [client]
-  (fn [req]
-    (if-let [params (:params req)]
-      (if (query-string-methods (keyword (:request-method req)))
-        (client (-> req
-                    (dissoc :params)
-                    (assoc :query-params params)))
-        (client (-> req
-                    (dissoc :params)
-                    (assoc :form-params params))))
-      (client req))))
-
-(defn wrap-request
-  "Wraps a request function with an appropriate stack of middleware."
-  [request]
-  (-> request
-      clj-http.client/wrap-request-timing
-      wrap-retry-on-throttle-errors
-      clj-http.client/wrap-lower-case-headers
-      ; clj-http.client/wrap-query-params
-      wrap-query-params
-      ; clj-http.client/wrap-basic-auth
-      ; clj-http.client/wrap-oauth
-      ; clj-http.client/wrap-user-info
-      ; clj-http.client/wrap-url
-      ; clj-http.client/wrap-redirects
-      clj-http.client/wrap-decompression
-      clj-http.client/wrap-input-coercion
-      ; put this before output-coercion, so additional charset
-      ; headers can be used if desired
-      clj-http.client/wrap-additional-header-parsing
-      clj-http.client/wrap-output-coercion
-      clj-http.client/wrap-exceptions
-      clj-http.client/wrap-accept
-      clj-http.client/wrap-accept-encoding
-      clj-http.client/wrap-content-type
-      clj-http.client/wrap-form-params
-      ; clj-http.client/wrap-nested-params
-      wrap-generic-params-key
-      clj-http.client/wrap-method
-      ; clj-http.cookies/wrap-cookies
-      clj-http.links/wrap-links
-      ; clj-http.client/wrap-unknown-host
-      wrap-json-format
-      wrap-access-token
-      wrap-shop
-      wrap-ssl))
 
 (def request
   ^{:doc "Makes a request to the Shopify API."}
-  (wrap-request clj-http.core/request))
+  middleware/request)
 
 (defn collection-name
   "Converts resource keywords to their plural forms, unless it's a singleton resource (e.g. `:shop`)."
@@ -182,6 +18,7 @@ In the response:
       ("country" "countries") "countries"
       ("shop" "shops") "shop"
       (str/replace-first resource #"s?$" "s"))))
+
 (def collection-keyword (comp keyword collection-name))
 
 (defn member-name
@@ -191,6 +28,7 @@ In the response:
     (case resource
       ("country" "countries") "country"
       (str/replace-first resource #"s?$" ""))))
+
 (def member-keyword (comp keyword member-name))
 
 (def path-params
---
+++ shopify-resources/test/shopify/resources/client_middleware_test.clj
@@ -0,0 +1,157 @@
+(ns shopify.resources.client-middleware-test
+  (:use clojure.test
+        [shopify.resources.client-middleware :as middleware]))
+
+(def default-session
+  {:shop "xerxes.myshopify.com"
+   :access-token "e5ea7fb51ff27a20c3f622df66b9acdc"})
+
+(deftest params->query-string-test
+  (testing "(params->query-string params) produces a nested query string using square bracket syntax"
+    (is (= "asset[key]=templates%2Findex.liquid"
+           (params->query-string {:asset {:key "templates/index.liquid"}})))
+    (is (= "nest[num]=123&nest[all][]=this&nest[all][][silly]=stuff&nest[all][]=here"
+           (params->query-string {:nest {:all ["this" {:silly :stuff} "here"] :num 123}})))))
+
+(deftest wrap-json-format-middleware-test
+  (testing "(wrap-json-format clj-http-client) wraps the client to set appropriate :accept and :as keys, and append .json format to paths"
+    (let [wrapped-identity (wrap-json-format identity)]
+      (is (= {:query-params {:foo "bar"}
+              :accept :json
+              :as :json
+              :content-type :json}
+             (wrapped-identity {:query-params {:foo "bar"}})))
+      (is (= {:uri "/admin/products.json"
+              :accept :json
+              :as :json
+              :content-type :json}
+             (wrapped-identity {:uri "/admin/products"})))
+      (is (= {:uri "/admin/products.json"
+              :accept :json
+              :as :json
+              :content-type :json}
+             (wrapped-identity {:uri "/admin/products.json"}))))))
+
+(deftest wrap-access-token-middleware-test
+  (testing "(wrap-access-token clj-http-client) wraps the client to convert an :access-token key into the appropriate auth header"
+    (let [wrapped-identity (wrap-access-token identity)]
+      (is (= {:shop "xerxes.myshopify.com"
+              :headers {"x-shopify-access-token" "e5ea7fb51ff27a20c3f622df66b9acdc"}}
+             (wrapped-identity default-session)))
+      (is (= {:shop "xerxes.myshopify.com"
+              :headers {"foo" "bar"
+                        "x-shopify-access-token" "e5ea7fb51ff27a20c3f622df66b9acdc"}}
+             (wrapped-identity (-> default-session
+                                   (assoc :headers {"foo" "bar"}))))))))
+
+(deftest wrap-shop-middleware-test
+  (testing "(wrap-shop clj-http-client) wraps the client to change :shop keys into :server-name keys"
+    (let [wrapped-identity (wrap-shop identity)]
+      (is (= {:server-name "xerxes.myshopify.com"
+              :access-token "e5ea7fb51ff27a20c3f622df66b9acdc"}
+             (wrapped-identity default-session))))))
+
+(deftest wrap-ssl-middleware-test
+  (testing "(wrap-ssl clj-http-client) wraps the client to default :scheme to :https"
+    (let [wrapped-identity (wrap-ssl identity)]
+      (is (= {:scheme :https
+              :shop "xerxes.myshopify.com"
+              :access-token "e5ea7fb51ff27a20c3f622df66b9acdc"}
+             (wrapped-identity default-session)))
+      (is (= {:scheme :http
+              :shop "xerxes.myshopify.com"
+              :access-token "e5ea7fb51ff27a20c3f622df66b9acdc"}
+             (wrapped-identity (assoc default-session :scheme :http)))))))
+
+(deftest wrap-retry-on-throttle-errors-middleware-test
+  (let [throttled-response {:status 429 :headers {} :body ""}
+        ok-response {:status 200 :headers {} :body ""}
+        mock-responder (fn [failure-count]
+                         (let [counter (atom failure-count)]
+                           (fn [req]
+                             (if (= 0 @counter)
+                               ok-response
+                               (do
+                                 (swap! counter dec)
+                                 throttled-response)))))]
+    (testing "wrap-retry-on-throttle-errors middleware doesn't do anything when :retry-on-throttle-errors is false"
+      (let [fail-once (wrap-retry-on-throttle-errors (mock-responder 1))]
+        (is (= throttled-response
+               (fail-once {:throttle-retry-delay 0.1
+                           :retry-on-throttle-errors false})))))
+    (testing "wrap-retry-on-throttle-errors middleware doesn't retry on successful response"
+      (let [succeed (wrap-retry-on-throttle-errors (constantly ok-response))]
+        (is (= ok-response
+               (succeed {:throttle-retry-delay 0.1})))))
+    (testing "wrap-retry-on-throttle-errors middleware tells you how many times it had to retry, if not zero"
+      (let [fail-twice (wrap-retry-on-throttle-errors (mock-responder 2))]
+        (is (= (assoc ok-response
+                 :throttle-retry-count 2)
+               (fail-twice {:throttle-retry-delay 0.1
+                            :max-throttle-retries 2})))))
+    (testing "wrap-retry-on-throttle-errors middleware returns the last failed response if it hits the max number of retries"
+      (let [fail-5-times (wrap-retry-on-throttle-errors (mock-responder 5))]
+        (is (= (assoc throttled-response
+                 :throttle-retry-count 4)
+               (fail-5-times {:throttle-retry-delay 0.1
+                              :max-throttle-retries 4})))))))
+
+(deftest wrap-generic-params-key-test
+  (testing "wrap-generic-params-key is middleware which converts a :params key to either :query-params or :form-params depending on request method"
+    (let [wrapped-identity (wrap-generic-params-key identity)]
+      (is (= {:request-method :get
+              :query-params {:foo "bar"}}
+             (wrapped-identity {:request-method :get
+                                :params {:foo "bar"}})))
+      (is (= {:request-method :post
+              :form-params {:foo "bar"}}
+             (wrapped-identity {:request-method :post
+                                :params {:foo "bar"}}))))))
+
+(deftest wrap-all-middleware-test
+  (testing "(wrap-request clj-http-client) wraps the client fn in the appropriate middleware for Shopify resource requests, transforming the requests and responses correctly"
+    (let [raw-page-count-response {:status 200
+                                   :headers {"x-ua-compatible" "IE=Edge,chrome=1"
+                                             "server" "nginx"
+                                             "x-runtime" "0.044928"
+                                             "content-encoding" "gzip"
+                                             "p3p" "CP=\"NOI DSP COR NID ADMa OPTa OUR NOR\""
+                                             "content-type" "application/json; charset=utf-8"
+                                             "x-request-id" "362bb5e86ffa2fc3fd11df72a8ac33d6"
+                                             "date" "Sat, 05 Jan 2013 05:10:40 GMT"
+                                             "http_x_shopify_shop_api_call_limit" "2/500"
+                                             "cache-control" "max-age=0, private, must-revalidate"
+                                             "vary" "Accept-Encoding"
+                                             "status" "200 OK"
+                                             "transfer-encoding" "chunked"
+                                             "etag" "\"a18b769e721d2462b4d3057ffe7ac0c0\""
+                                             "x-shopify-shop-api-call-limit" "2/500"
+                                             "connection" "close"}
+                                   :body (byte-array (map byte [31, -117, 8, 0, 0, 0, 0, 0, 0, 3, -85, 86, 74, -50, 47, -51, 43, 81, -78, 50, -86, 5, 0, 106, 49, -10, -15, 11, 0, 0, 0]))}
+          request (-> default-session
+                      (assoc
+                        :method :get
+                        :uri "/admin/pages/count"
+                        :params {:since_id 108828309}))
+          wrapped-assertions (wrap-request
+                               (fn [req]
+                                 (is (= {:scheme :https
+                                         :request-method :get
+                                         :content-type :json
+                                         :as :json
+                                         :uri "/admin/pages/count.json"
+                                         :query-string "since_id=108828309"
+                                         :server-name "xerxes.myshopify.com"
+                                         :headers
+                                         {"accept-encoding" "gzip, deflate"
+                                          "accept" "application/json"
+                                          "content-type" "application/json"
+                                          "x-shopify-access-token" "e5ea7fb51ff27a20c3f622df66b9acdc"}}
+                                        req))
+                                 raw-page-count-response))
+          response (wrapped-assertions request)]
+      (is (= {:count 2}
+             (:body response)))
+      (is (= (:headers raw-page-count-response)
+             (:headers response))))))
+
