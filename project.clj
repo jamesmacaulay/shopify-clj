c66fe62d3857bf4b3e67370828a060374bcf31be
James MacAulay <james@shopify.com>
2013-03-10 18:42:02 -0400

    sub-projects are annoying

--- project.clj
+++ project.clj
@@ -1,13 +1,18 @@
 (defproject shopify "0.1.0-SNAPSHOT"
-  :description "A set of libraries for interacting with the Shopify platform"
+  :description "A library for interacting with the Shopify platform."
   :url "https://github.com/jamesmacaulay/shopify-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[shopify/shopify-core "0.1.0-SNAPSHOT"]
-                 [shopify/shopify-friend "0.1.0-SNAPSHOT"]
-                 [shopify/shopify-resources "0.1.0-SNAPSHOT"]]
-  :plugins [[lein-sub "0.2.4"]
-            [lein-marginalia "0.7.1"]]
-  :sub ["shopify-core"
-        "shopify-friend"
-        "shopify-resources"])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.flatland/useful "0.9.4"]
+                 [prismatic/plumbing "0.0.1"]
+                 [digest "1.4.0"]
+                 [clj-http "0.6.3"]
+                 [com.cemerick/friend "0.1.2"]]
+  :profiles {:dev {:dependencies [[ring-mock "0.1.3"]
+                                  [org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.5"]}
+  :plugins [[lein-marginalia "0.7.1"]
+            [codox "0.6.4"]]
+  :codox {:output-dir "docs/codox"})
